68fc38e82502d21834e642ba211f760dda2b0887
Chris Gullickson <cgullickson5@gmail.com>
2018-02-08 18:00:16 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180208174528) do
+ActiveRecord::Schema.define(version: 20180208175918) do
+
+  create_table "post_tags", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "tag_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "name"
